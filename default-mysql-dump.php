72353a41fea2218d111ce061aaf3f4ce14e892ab
Alfi Gusman <alfigusman0@gmail.com>
2022-09-21 17:06:37 +0700

    update

--- default-mysql-dump.php
+++ default-mysql-dump.php
@@ -3,11 +3,19 @@ date_default_timezone_get('Asia/Jakarta');
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-$database = '';
-$user = 'root';
+$database = array(
+    "db_database",
+);
+$user = '';
 $pass = '';
 $host = 'localhost';
-$dir = dirname(__FILE__) . '/sql/'.$database.'_'.date('YmdHis').'.sql';
-echo "<h3>Backing up database to `<code>{$dir}</code>`</h3>";
-exec("mysqldump --user={$user} --password={$pass} --host={$host} {$database} --result-file={$dir} 2>&1", $output);
-var_dump($output);
+for ($i = 0; $i < sizeof($database); $i++) {
+    $dir = dirname(__FILE__) . '/sql/' . $database[$i] . '/';
+    if (!file_exists($dir)) {
+        mkdir($dir, 0777, true);
+    }
+    $dir .= $database[$i] . '_' . date('YmdHis') . '.sql';
+    echo "<h3>Backing up database to `<code>{$dir}</code>`</h3>";
+    exec("mysqldump --user={$user} --password={$pass} --host={$host} {$database[$i]} -R -K --triggers --result-file={$dir} 2>&1", $output);
+    var_dump($output);
+}
